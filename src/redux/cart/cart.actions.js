c4522d524a5835d56949b57e4a2b955410bad3b8
Andres De La Barra <adelabarra24@gmail.com>
2020-03-12 17:03:19 -0400

    add functionality to add unique items to cart

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -3,4 +3,9 @@ import CartActionTypes from './cart.types';
 // payload is optional, since its not needed here
 export const toggleCartHidden = () => ({
   type: CartActionTypes.TOGGLE_CART_HIDDEN,
-})
+});
+
+export const addItem = (item) => ({
+  type: CartActionTypes.ADD_ITEM,
+  payload: item
+});
