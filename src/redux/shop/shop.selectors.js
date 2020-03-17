9478353c89f80ff9c89a7b25529f59116a5e8454
Andres De La Barra <adelabarra24@gmail.com>
2020-03-17 02:09:12 -0400

    moved directory ans shop into own reducer and created respective selectors

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -7,3 +7,13 @@ export const selectCollections = createSelector(
   shop => shop.collections
 );
 
+export const selectCollection = collectionUrlParam => 
+  createSelector(
+    [selectCollections],
+    collections => collections[collectionUrlParam]
+  );
+
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  collections => Object.keys(collections).map(key => collections[key])
+);
